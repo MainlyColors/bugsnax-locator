822674f197be4b18da206c909b0b92134ff0dea3
Ryan <ryanmahara@gmail.com>
2022-04-15 13:28:47 -0500

    replace template info with new info, add JSON data to get around CORS error

--- js/main.js
+++ js/main.js
@@ -3,18 +3,40 @@ document.querySelector('button').addEventListener('click', getFetch);
 
 const quoteEl = document.querySelector('#quote');
 const characterEl = document.querySelector('#character');
+
+function createBugsnaxLiElement(name, location) {
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  const pBug = document.createElement('p');
+  const pLoc = document.createElement('p');
+
+  div.classList.add('list-entry');
+
+  pBug.textContent = name;
+  pLoc.textContent = location;
+
+  div.appendChild(pBug);
+  div.appendChild(pLoc);
+
+  li.appendChild(div);
+
+  return li;
+}
+
 function getFetch() {
-  const url = `https://animechan.vercel.app/api/random`;
+  // const url = `https://www.bugsnaxapi.com/api/bugsnax`;
+  const url = '../JSON/bugsnax.json';
 
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
-    .then(({ anime, character, quote }) => {
-      // console.log(data);
-
-      quoteEl.textContent = quote;
-      characterEl.textContent = `${character} (${anime})`;
+    .then((data) => {
+      console.log(data);
     })
     .catch((err) => {
       console.log(`error ${err}`);
     });
 }
+// { headers: { 'Access-Control-Allow-Origin': '*' } }
+// {
+//   headers: { 'Access-Control-Allow-Origin': 'http://127.0.0.1:5500/' },
+// }
